a7e2b304306aae9cc964b664a507210862067afd
Chris Hjorth <contact@chrishjorth.com>
2014-08-16 09:11:11 +0200

    Git hook test

--- app/test/api.spec.js
+++ app/test/api.spec.js
@@ -55,7 +55,7 @@ describe('API', function() {
 				throw err;
 			}
 			expect(res.headers['content-type']).to.equal('application/json');
-			expect(res.body).to.be.an('object');
+			expect(res.body).to.be.an('objec');
 			done();
 		});
 	});
