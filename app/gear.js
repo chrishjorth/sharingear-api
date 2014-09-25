97f1257f4f83f251415e9695f56eb49b7c414e21
Chris Hjorth <contact@chrishjorth.com>
2014-09-25 15:17:33 +0200

    Live debugging.

--- app/gear.js
+++ app/gear.js
@@ -284,6 +284,10 @@ function search(lat, lng, gear, callback) {
 		sql += rows[rows.length - 1].id; //rows has at least one item
 		sql += ") AND distance <= 10000.0 ORDER BY distance ASC LIMIT 100";
 		console.log('Search location');
+		console.log('SQL:');
+		console.log(sql);
+		console.log('lat: ' + lat);
+		console.log('lng: ' + lng);
 		db.search(sql, [lat, lng], function(error, rows) {
 			if(error) {
 				console.log('search error: ' + JSON.stringify(error));
