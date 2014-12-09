e16a3f3faefa4d2de127125b209f7e466f7e8cfc
Chris Hjorth <contact@chrishjorth.com>
2014-12-09 16:40:55 +0100

    Removed old code.

--- app/booking.js
+++ app/booking.js
@@ -24,7 +24,6 @@ var db = require("./database"),
 
 	create,
 	read,
-	//readClosest,
 	readRentalsForUser,
 	readReservationsForUser,
 	update,
@@ -97,20 +96,6 @@ read = function(bookingID, callback) {
 		callback(null, rows[0]);
 	});
 };
-
-/*readClosest = function(gearID, callback) {
-	db.query("SELECT id, gear_id, MIN(start_time) as start_time, end_time, renter_id, price, booking_status FROM bookings WHERE gear_id=? AND booking_status!='ended-denied' AND booking_status!='ended'", [gearID], function(error, rows) {
-		if(error) {
-			callback("Error selecting closest booking for gear " + gearID + ": " + error);
-			return;
-		}
-		if(rows.length <= 0) {
-			callback("No bookings for gear with id " + gearID + ".");
-			return;
-		}
-		callback(null, rows[0]);
-	});
-};*/
 
 readRentalsForUser = function(userID, callback) {
 	Gear.checkForRentals(userID, function(error) {
@@ -293,7 +278,6 @@ endBooking = function(gearID, price, callback) {
 module.exports = {
 	create: create,
 	read: read,
-	//readClosest: readClosest,
 	readRentalsForUser: readRentalsForUser,
     readReservationsForUser: readReservationsForUser,
 	update: update
