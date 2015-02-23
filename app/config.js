8a8b56c18144934c1fdab12fdaa2a82990ad204d
Chris Hjorth <contact@chrishjorth.com>
2015-02-23 11:50:28 +0100

    Fixed sharingear payment user creation bug.

--- app/config.js
+++ app/config.js
@@ -7,8 +7,9 @@
 "use strict";
 
 var IS_PRODUCTION = false, //This variable should be set and saved according to the git branch: true for master and false for develop
-	fs = require('fs'),
+	fs = require("fs"),
 	SEARCH_RADIUS = 80000,
+	SHARINGEAR_VAT = 25.0,
 	MYSQL_URL,
 	MYSQL_CA,
 	MYSQL_CERT,
@@ -63,5 +64,6 @@ module.exports = {
 	MANGOPAY_USERNAME: MANGOPAY_USERNAME,
 	MANGOPAY_SANDBOX_KEY: MANGOPAY_SANDBOX_KEY,
 	SEARCH_RADIUS: SEARCH_RADIUS,
+	SHARINGEAR_VAT: SHARINGEAR_VAT,
 	isProduction: isProduction
 };
