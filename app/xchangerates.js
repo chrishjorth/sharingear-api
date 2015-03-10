680e25dfbc176826431f2fa42b0437ae87357c41
Chris Hjorth <contact@chrishjorth.com>
2015-03-10 22:11:56 +0100

    Added failsafe for when Yahoo API xchange rates are failing.

--- app/xchangerates.js
+++ app/xchangerates.js
@@ -2,6 +2,7 @@
  * Defines a currency conversion module based on exchange rates from Yahoo.
  * YML query builder: https://developer.yahoo.com/yql/console/?q=show%20tables&env=store://datatables.org/alltableswithkeys#h=select+*+from+yahoo.finance.xchange+where+pair+in+(%22EURUSD%22)
  * Example REST call: https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22EURUSD%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
+ * Consider using https://openexchangerates.org/signup
  * @author: Chris Hjorth
  */
 
@@ -13,7 +14,8 @@ var https = require("https"),
 	getRate,
 	getYahooRate,
 	getFixerRate,
-	getJSON;
+	getECBRate,
+	getData;
 
 getRate = function(fromCurrency, toCurrency, callback) {
 	var code = fromCurrency + toCurrency,
@@ -30,16 +32,16 @@ getRate = function(fromCurrency, toCurrency, callback) {
 
 	getYahooRate(fromCurrency, toCurrency, function(error, yahooRate) {
 		if(error) {
-			getFixerRate(fromCurrency, toCurrency, function(error, fixerRate) {
+			getECBRate(fromCurrency, toCurrency, function(error, ecbRate) {
 				if(error) {
 					callback(error);
 					return;
 				}
 				currencies[code] = {
-					rate: fixerRate,
+					rate: ecbRate,
 					timestamp: Date.now
 				};
-				callback(null, fixerRate);
+				callback(null, ecbRate);
 			});
 		}
 		else {
@@ -55,9 +57,6 @@ getRate = function(fromCurrency, toCurrency, callback) {
 getYahooRate = function(fromCurrency, toCurrency, callback) {
 	var code = fromCurrency + toCurrency,
 		query, path;
-
-	callback("blaaaah");
-	return;
 	
 	query = "select * from yahoo.finance.xchange where pair in (\"";
 	query += code;
@@ -65,32 +64,99 @@ getYahooRate = function(fromCurrency, toCurrency, callback) {
 
 	path = "/v1/public/yql?q=" + encodeURI(query);
 
-	getJSON("query.yahooapis.com", path, function(error, data) {
-		var rate;
+	getData("query.yahooapis.com", path, function(error, data) {
+		var rate, parsedData;
 		if(error) {
 			callback("Error retrieving exchange rate: " + error);
 			return;
 		}
-		rate = parseFloat(data.query.results.rate.Rate);
+		try {
+			parsedData = JSON.parse(data);
+		}
+		catch(error) {
+			callback(error.message);
+			return;
+		}
+		rate = parseFloat(parsedData.query.results.rate.Rate);
 		callback(null, rate);
 	});
 };
 
 getFixerRate = function(fromCurrency, toCurrency, callback) {
 	var query = "/latest?symbols=" + fromCurrency + "," + toCurrency;
-	getJSON("api.fixer.io", query, function(error, data) {
-		var rate;
+	getData("api.fixer.io", query, function(error, data) {
+		var rate, parsedData;
 		if(error) {
 			callback("Error retrieving exchange rate: " + error);
 			return;
 		}
-		console.log("BOOM!");
-		rate = parseFloat(data.rates[toCurrency]);
+		try {
+			parsedData = JSON.parse(data);
+		}
+		catch(error) {
+			callback(error.message);
+			return;
+		}
+		rate = parseFloat(parsedData.rates[toCurrency]);
 		callback(null, rate);
 	});
 };
 
-getJSON = function(host, path, callback) {
+getECBRate = function(fromCurrency, toCurrency, callback) {
+	getData("www.ecb.europa.eu", "/stats/eurofxref/eurofxref-daily.xml", function(error, data) {
+		var i = 0,
+			fromRate = null,
+			toRate = null,
+			j, currency, line;
+		if(error) {
+			callback(error);
+			return;
+		}
+		while(i < data.length) {
+			j = data.indexOf("\n", i);
+			if(j === -1) {
+				j = data.length;
+			}
+			line = data.substr(i, j - i);
+			currency = line.match(/ currency='([^']*)'/);
+			if(currency !== null) {
+				currency = currency[1];
+				if(currency === fromCurrency) {
+					fromRate = line.match(/ rate='([^']*)'/);
+					if(fromRate !== null) {
+						fromRate = parseFloat(fromRate[1]);
+					}
+				}
+				if(currency === toCurrency) {
+					toRate = line.match(/ rate='([^']*)'/);
+					if(toRate !== null) {
+						toRate = parseFloat(toRate[1]);
+					}
+				}
+			}
+			i = j + 1;
+		}
+		if(fromCurrency === "EUR" && toCurrency === "EUR") {
+			callback(null, 1);
+			return;
+		}
+		if(fromCurrency === "EUR" && toRate !== null) {
+			callback(null, toRate);
+			return;
+		}
+		if(fromRate !== null && toCurrency === "EUR") {
+			callback(null, 1 / fromRate);
+			return;
+		}
+		if(fromRate !== null && toRate !== null) {
+			callback(null, 1 / fromRate * toRate);
+			return;
+		}
+		callback("Error getting currency conversion rate.");
+	});
+};
+
+getData = function(host, path, callback) {
 	var buffer = "",
 		options, request;
 
@@ -106,15 +172,8 @@ getJSON = function(host, path, callback) {
 		result.on("data", function(chunk) {
 			buffer += chunk;
 		});
-		result.on("end", function() {
-			var parsedData;
-			try {
-				parsedData = JSON.parse(buffer);
-				callback(null, parsedData);
-			}
-			catch(error) {
-				callback(error.message);
-			}
+		result.on("end", function() {	
+			callback(null, buffer);
 		});
 		result.on("error", function(e) {
 			callback(e.message);
