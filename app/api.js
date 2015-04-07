52b476a5fd1762c70e18664e4b36301449c42f80
Chris Hjorth <contact@chrishjorth.com>
2015-04-07 15:44:10 +0200

    Fixed handleError bug.

--- app/api.js
+++ app/api.js
@@ -183,12 +183,12 @@ readContentClassification = function(req, res, next) {
 		}
 		Vans.getClassification(function(error, vanClassification) {
 			if(error) {
-				handleError("Error retrieving van classification: " + error);
+				handleError(res, next, "Error retrieving van classification: " + error);
 				return;
 			}
 			Roadies.getClassification(function(error, roadieClassification) {
 				if(error) {
-					handleError("Error retrieving roadie classification: " + error);
+					handleError(res, next, "Error retrieving roadie classification: " + error);
 					return;
 				}
 				contentClassification = {
