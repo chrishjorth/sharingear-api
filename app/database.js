cf5497bd9e6f6c2826a9ba31e81c74c186c2375d
Chris Hjorth <contact@chrishjorth.com>
2015-05-02 15:04:57 +0200

    Added error handling for MySQL and Sphinx search pools.

--- app/database.js
+++ app/database.js
@@ -8,67 +8,75 @@
 "use strict";
 
 var mysql = require("mysql"),
-	Config = require("./config"),
-	sharingearPool,
-	sphinxPool,
+    Config = require("./config"),
+    sharingearPool,
+    sphinxPool,
 
-	query,
-	search;
+    query,
+    search;
 
 sharingearPool = mysql.createPool({
-	host: Config.MYSQL_URL,
-	port: 3306,
-	user: "root",
-	password: "20mircea14chris",
-	database: "sharingear",
-	supportBigNumbers: true, //Required for working with Facebook IDs stored as bigint.
-	multipleStatements: true, //Required for Minus operation from dynamic data set, which requires temp table
-	dateStrings: true,
-	acquireTimeout: 20000, //Default is 10000, we try with double to avoid occasional PROTOCOL_SEQUENCE_TIMEOUT errors
-	//connectTimeout: 60000, //Outcomment when not debugging with node inspector
-	ssl: {
-		ca: Config.MYSQL_CA,
-		cert: Config.MYSQL_CERT,
-		key: Config.MYSQL_KEY
-	}
+    host: Config.MYSQL_URL,
+    port: 3306,
+    user: "root",
+    password: "20mircea14chris",
+    database: "sharingear",
+    supportBigNumbers: true, //Required for working with Facebook IDs stored as bigint.
+    multipleStatements: true, //Required for Minus operation from dynamic data set, which requires temp table
+    dateStrings: true,
+    acquireTimeout: 20000, //Default is 10000, we try with double to avoid occasional PROTOCOL_SEQUENCE_TIMEOUT errors
+    //connectTimeout: 60000, //Outcomment when not debugging with node inspector
+    ssl: {
+        ca: Config.MYSQL_CA,
+        cert: Config.MYSQL_CERT,
+        key: Config.MYSQL_KEY
+    }
+});
+
+sharingearPool.on("error", function(error) {
+    console.error("Sharingear MySQL pool error: " + error.code);
 });
 
 sphinxPool = mysql.createPool({
-	host: Config.SPHINX_URL,
-	port: 9306
+    host: Config.SPHINX_URL,
+    port: 9306
+});
+
+sphinxPool.on("error", function(error) {
+    console.error("Sphinx SQL pool error: " + error.code);
 });
 
 query = function(queryString, paramArray, callback) {
-	sharingearPool.getConnection(function(error, connection) {
-		if(error) {
-			console.error("Error opening database connection.");
-			callback(error);
-			return;
-		}
-		connection.query(queryString, paramArray, function(error, rows) {
-			if(error) {
-				console.error("Error running query: " + queryString + ". " + error.code);
-			}
-			callback(error, rows);
-			connection.destroy();
-		});
-	});
+    sharingearPool.getConnection(function(error, connection) {
+        if (error) {
+            console.error("Error opening database connection.");
+            callback(error);
+            return;
+        }
+        connection.query(queryString, paramArray, function(error, rows) {
+            if (error) {
+                console.error("Error running query: " + queryString + ". " + error.code);
+            }
+            callback(error, rows);
+            connection.destroy();
+        });
+    });
 };
 
 search = function(searchString, paramArray, callback) {
-	sphinxPool.getConnection(function(error, connection) {
-		if(error) {
-			callback("Error opening sphinx connection: " + error);
-			return;
-		}
-		connection.query(searchString, paramArray, function(error, rows) {
-			callback(error, rows);
-			connection.destroy();
-		});
-	});
+    sphinxPool.getConnection(function(error, connection) {
+        if (error) {
+            callback("Error opening sphinx connection: " + error);
+            return;
+        }
+        connection.query(searchString, paramArray, function(error, rows) {
+            callback(error, rows);
+            connection.destroy();
+        });
+    });
 };
 
 module.exports = {
-	query: query,
-	search: search
+    query: query,
+    search: search
 };
