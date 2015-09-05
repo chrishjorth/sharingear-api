0a0169d8fd23325a979a797dfafd7263ff533d9a
Chris Hjorth <contact@chrishjorth.com>
2015-09-05 11:44:16 +0200

    Added prod AWS DB connection.

--- app/config.js
+++ app/config.js
@@ -26,10 +26,11 @@ var IS_PRODUCTION = false, //This variable should be set and saved according to 
 
 if(IS_PRODUCTION === true) {
 	DB_WIPEABLE = false; //Failsafe
-	MYSQL_URL = "173.194.246.188";
-	MYSQL_CA = fs.readFileSync(__dirname + "/../certificates/prod_server-ca.pem");
-	MYSQL_CERT = fs.readFileSync(__dirname + "/../certificates/prod_client-cert.pem");
-	MYSQL_KEY = fs.readFileSync(__dirname + "/../certificates/prod_client-key.pem");
+	//MYSQL_URL = "173.194.246.188";
+	MYSQL_URL = "mysql-prod.c4hmgqhmacxa.eu-west-1.rds.amazonaws.com";
+	//MYSQL_CA = fs.readFileSync(__dirname + "/../certificates/prod_server-ca.pem");
+	//MYSQL_CERT = fs.readFileSync(__dirname + "/../certificates/prod_client-cert.pem");
+	//MYSQL_KEY = fs.readFileSync(__dirname + "/../certificates/prod_client-key.pem");
 	SPHINX_URL = "130.211.79.103";
 	VALID_IMAGE_HOST = "www.sharingear.com";
 	MANGOPAY_SANDBOX_CLIENTID = "sharingear";
